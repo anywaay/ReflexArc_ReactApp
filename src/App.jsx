22a94a64fccac1371ee6df560c1d4da4878399d5
Ayush Tripathi <mailtoayushtripathi@gmail.com>
2024-06-02 13:24:14 +0530

    added functionalities to the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,42 @@ import "./App.css";
 
 const App = () => {
   const ref = useRef();
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [armPosition, setArmPosition] = useState([-14.301, 7.398, 9.125]); 
   const [modelPosition, setModelPosition] = useState([0, 0.9, 0]);
   const [modelScale, setModelScale] = useState([3.5, 3.5, 3.5]);
   const [initialCameraTarget, setInitialCameraTarget] = useState([0, 0, 0]);
 
+  const handleAnimate = () => {
+    setIsAnimating(true);
+  };
+
+  const handleReset = () => {
+    setArmPosition([-14.301, 7.398, 9.125]);
+    setIsAnimating(false);
+  };
+
+  const handleClear = () => {
+    setInitialCameraTarget([0, 20, 0]); 
+    setArmPosition([-14.301, 7.398, 9.125]);
+    setModelPosition([0, 0.9, 0]);
+    setIsAnimating(false);
+  };
+  
   useEffect(() => {
     const handleKeyDown = (event) => {
       switch (event.key) {
-        case 'ArrowUp':
+        case 'w':
           setModelPosition(prevPosition => [prevPosition[0], prevPosition[1] - 0.5, prevPosition[2]]);
           break;
-        case 'ArrowDown':
+        case 's':
           setModelPosition(prevPosition => [prevPosition[0], prevPosition[1] + 0.5, prevPosition[2]]);
           break;
-        case 'ArrowLeft':
-          setModelPosition(prevPosition => [prevPosition[0] , prevPosition[1], prevPosition[2]- 0.5]);
+        case 'a':
+          setModelPosition(prevPosition => [prevPosition[0], prevPosition[1], prevPosition[2] - 0.5]);
           break;
-        case 'ArrowRight':
-          setModelPosition(prevPosition => [prevPosition[0] , prevPosition[1], prevPosition[2]+ 0.5]);
+        case 'd':
+          setModelPosition(prevPosition => [prevPosition[0], prevPosition[1], prevPosition[2] + 0.5]);
           break;
         default:
           break;
@@ -37,16 +55,17 @@ const App = () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, []);
+
   return (
     <div className="app-container">
       <h1>Simulation of Reflex Arc</h1>
       <div className="canvas-container">
-      <ErrorBoundary>
-          <Canvas className="canvas" shadows dpr={[1, 2]} camera={{ position: [50, 0, 0], fov: 35 }}>
+        <ErrorBoundary>
+          <Canvas className="canvas" shadows dpr={[1, 2]} camera={{ position: [50, 10, 10], fov: 35 }}>
             <Suspense fallback={null}>
               <directionalLight position={[1, 5, 1]} intensity={4.5}/>
               <ambientLight intensity={0.5} />
-              <ReflexArc position={modelPosition} scale={modelScale} />
+              <ReflexArc position={modelPosition} scale={modelScale} arm={armPosition} isAnimating={isAnimating} />
             </Suspense>
             <OrbitControls ref={ref} target={initialCameraTarget} />
           </Canvas>
@@ -54,9 +73,9 @@ const App = () => {
       </div>
       <div className="buttons-container">
         <button className="button">Update</button>
-        <button className="button">Reset</button>
-        <button className="button">Clear</button>
-        <button className="button">Animate</button>
+        <button className="button" onClick={handleReset}>Reset Arm</button>
+        <button className="button" onClick={handleClear}>Clear</button>
+        <button className="button" onClick={handleAnimate}>Animate</button>
       </div>
     </div>
   );
